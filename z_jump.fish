ba1b51e14e31930ab8d8c32de5ac0f0dd1a9b140
ymmtmdk <ymmtmdk@gmail.com>
2017-03-29 18:31:46 +0900

    first commit

---
+++ z_jump.fish
@@ -0,0 +1,10 @@
+set Z_SCRIPT_PATH /usr/local/etc/profile.d/z.sh
+
+function z_jump.check --on-variable PWD --description 'Setup z on directory change'
+  status --is-command-substitution; and return
+  bash -c "source $Z_SCRIPT_PATH; _z --add `pwd -P`"
+end
+
+function z_jump -d "jump around"
+  cd (bash -c "source $Z_SCRIPT_PATH; _z $argv; echo \$PWD")
+end
